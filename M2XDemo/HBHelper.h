1ca4f770807ac85092b82479f008f3d82acbc3c9
Luis Floreani <luis.floreani@gmail.com>
2015-01-15 12:00:15 -0300

    text fields and buttons added to support dynamic type

--- M2XDemo/HBHelper.h
+++ M2XDemo/HBHelper.h
@@ -7,10 +7,13 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface HBHelper : NSObject
 
 // since sorting in swift is horrendous slow...
 + (NSArray *)sortValues:(NSArray *)values;
+
++ (void)configureDynamicTypeFor:(id)control keypath:(NSString *)keypath;
 
 @end
